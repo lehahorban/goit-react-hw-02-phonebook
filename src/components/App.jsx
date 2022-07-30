f69a6d6bfb22e36a1d6b9baebdfd4f4e3c047467
Oleksii Horban <lehagorban93@gmail.com>
2022-07-30 23:45:27 +0300

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,13 @@ import shortid from 'shortid';
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
     name: '',
     number: '',
   };
@@ -34,7 +40,7 @@ class App extends Component {
   };
 
   render() {
-    const { name, number } = this.state;
+    const { name, number, filter } = this.state;
     return (
       <div>
         <p>Phonebook</p>
@@ -49,6 +55,7 @@ class App extends Component {
             onChange={this.onChange}
             value={name}
           />
+          <label>Number</label>
           <input
             type="tel"
             name="number"
@@ -57,6 +64,13 @@ class App extends Component {
             required
             onChange={this.onChange}
             value={number}
+          />
+          <label>Find contact by name</label>
+          <input
+            type="text"
+            name="filter"
+            value={filter}
+            onChange={this.onChange}
           />
           <button type="submit">Add contact</button>
         </form>
